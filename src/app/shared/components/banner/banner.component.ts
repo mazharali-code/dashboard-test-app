c1b9b59d9780789ee782ff0b763f1f476d6f8fe7
Mazhar Ali <mazharali@live.com>
2026-02-13 03:00:45 +0000

    Configure bootstrap and add content to banner component

--- src/app/shared/components/banner/banner.component.ts
+++ src/app/shared/components/banner/banner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   standalone: true,
 })
 export class BannerComponent {
-
+  @Input() title: string = '';
+  @Input() subtitle: string = '';
 }
